158e82421239c5bdea58499bb9851b25eca28598
Ludvig Sundstrom <ludvig.sundstroem@innoq.com>
2020-12-12 15:24:33 +0100

    Minimize project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject drums "0.1.0-SNAPSHOT"
-  :plugins [[lein-cljsbuild "1.1.8"]
-            [lein-figwheel "0.5.20"]]
+  :plugins [[lein-figwheel "0.5.20"]]
   :description "Drum machine"
   :url "https://github.com/lsund/drums"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -14,24 +13,14 @@
   :resource-paths ["resources"]
   :clean-targets ^{:protect false} [:target-path]
 
-  :profiles {:dev {:cljsbuild
-                   {:builds {:client
-                             {:figwheel {:on-jsload "drums.core/run"}
-                              :compiler {:main "drums.core"
-                                         :optimizations :none}}}}}
-
-             :prod {:cljsbuild
-                    {:builds {:client
-                              {:compiler {:optimizations :advanced
-                                          :elide-asserts true
-                                          :pretty-print false}}}}}}
-
   :figwheel {:repl false
              :css-dirs ["resources/public"]
              :http-server-root "public"}
 
   :cljsbuild {:builds {:client
                        {:source-paths ["src"]
+                        :figwheel {:on-jsload "drums.core/run"}
                         :compiler {:output-dir "resources/public/client"
+                                   :main "drums.core"
                                    :asset-path "client"}}}})
 
