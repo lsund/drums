bdf3708d61308af9860b6988d8c68a2524a1c05d
Ludvig Sundstrom <ludvig.sundstroem@innoq.com>
2020-12-12 16:35:11 +0100

    Add aot

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :url "https://github.com/lsund/drums"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :profiles {:uberjar {:aot :all}}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.764"]
                  [org.clojure/core.async "1.3.610"]
